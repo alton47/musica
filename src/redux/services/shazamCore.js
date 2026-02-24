be865b60a6be2d5a0c832196213649f5e35304a1
Allan <altonallan47@gmail.com>
2026-02-24 21:52:40 +0300

    Create a shazamAPI

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,3 +1,5 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 const options = {
   method: "GET",
   headers: {
@@ -10,3 +12,6 @@ fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.log(err));
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+});
