37b586edb9baf5c0f70ab18def327d8113647e89
Allan <altonallan47@gmail.com>
2026-02-25 16:36:05 +0300

    feat(player): build PlayerBar UI shell (bottom bar)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { songsData } from "../assets/assets";
+import { songsData, assets } from "../assets/assets";
 
 const Player = () => {
   return (
@@ -9,6 +9,41 @@ const Player = () => {
           <p>{songsData[0].name}</p>
           <p>{songsData[0].desc.slice(0, 12)}</p>
         </div>
+      </div>
+
+      <div className="flex flex-col items-center gap-1 m-auto">
+        <div className="flex gap-4">
+          <img
+            className="w-4 cursor-pointer"
+            src={assets.shuffle_icon}
+            alt=""
+          />
+          <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
+        </div>
+        <div className="flex items-center gap-5">
+          <p>1:08</p>
+          <div className="w-[60vw] max-w-125 bg-gray-300 rounded-full cursor-pointer">
+            <hr className="h-1 border-none w-5 bg-green-800 rounded-full" />
+          </div>
+          <p>3:20</p>
+        </div>
+      </div>
+
+      <div className="hidden lg:flex items-center gap-2 opacity-75">
+        <img className="w-4 cursor-pointer" src={assets.plays_icon} alt="" />
+        <img className="w-4 cursor-pointer" src={assets.mic_icon} alt="" />
+        <img className="w-4 cursor-pointer" src={assets.queue_icon} alt="" />
+        <img className="w-4 cursor-pointer" src={assets.speaker_icon} alt="" />
+        <img className="w-4 cursor-pointer" src={assets.volume_icon} alt="" />
+        <img
+          className="w-4 cursor-pointer"
+          src={assets.mini_player_icon}
+          alt=""
+        />
+        <img className="w-4 cursor-pointer" src={assets.zoom_icon} alt="" />
       </div>
     </div>
   );
