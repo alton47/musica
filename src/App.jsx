66091cbe2450b7d43d3f62ad1e8acf13b773ec00
Allan <altonallan47@gmail.com>
2026-02-25 15:23:07 +0300

    refactor : Clean slate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-9xl text-blue-300">matakoo</div>;
+  return <div className="text-9xl text-blue-300 mt-50 ml-30">matakoo</div>;
 };
 
 export default App;
