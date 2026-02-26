17bbf636ec64c9e71f33067043f6cde83b5c8512
Allan <altonallan47@gmail.com>
2026-02-26 22:06:50 +0300

    feat(player): Implement the Play functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,15 +3,16 @@ import { songsData, assets } from "../assets/assets";
 import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
-  const { seekBar, seekBg } = useContext(PlayerContext);
+  const { seekBar, seekBg, playStatus, play, pause } =
+    useContext(PlayerContext);
 
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
       <div className="hidden lg:flex items-center gap-4">
         <img className="w-12" src={songsData[0].image} alt="" />
         <div>
-          <p>{songsData[0].name}</p>
-          <p>{songsData[0].desc.slice(0, 12)}</p>
+          <p>{track.name}</p>
+          <p>{track.desc.slice(0, 12)}</p>
         </div>
       </div>
 
@@ -23,7 +24,22 @@ const Player = () => {
             alt=""
           />
           <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
-          <img className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+          {playStatus ? (
+            <img
+              onClick={pause}
+              className="w-4 cursor-pointer"
+              src={assets.pause_icon}
+              alt=""
+            />
+          ) : (
+            <img
+              onClick={play}
+              className="w-4 cursor-pointer"
+              src={assets.play_icon}
+              alt=""
+            />
+          )}
+
           <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
           <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
